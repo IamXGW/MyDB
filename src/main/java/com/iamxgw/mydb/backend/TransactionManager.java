31d3ad770b0c70acf29694af0c281517e00d23de
IamXGW <iamxgw@gmail.com>
2022-10-25 20:58:39 +0800

    init repo

---
+++ src/main/java/com/iamxgw/mydb/backend/TransactionManager.java
@@ -0,0 +1,18 @@
+package com.iamxgw.mydb.backend;
+
+public interface TransactionManager {
+    // 开启新事务
+    long bagin();
+    // 提交事务
+    void commit(long xid);
+    // 取消事务
+    void abort(long xid);
+    // 查询一个事务是不是正在进行的状态
+    boolean isActivate(long xid);
+    // 查询一个事务的状态是否已提交
+    boolean isCommitted(long xid);
+    // 查询一个事务的状态是否已取消
+    boolean isAborted(long xid);
+    // 关闭 TM TransactionManager
+    void close();
+}
